2971422c9ee6b15fa499a13385bac94b31936569
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-03-06 23:32:43 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,19 @@
-function flip(array, n) {
-    return array;
+function getBiggest(a, length) {
+    let b = a.slice(0,length)
+    return b.indexOf(Math.max(...b))
 }
-
-// Use only flip() here to manipulate the array
-function pancakeSort(array) {
-    return array;
+function flip(a, n) {
+    if (n <= 1) return
+    else if (n == 1) [a[0], a[1]] = [a[1], a[0]]
+    else if (n >= a.length) for (let i = 0, j = a.length-1; i < j; i++, j--) [a[i], a[j]] = [a[j], a[i]]
+    else for (let i = 0, j = n-1; i < j; i++, j--) [a[i], a[j]] = [a[j], a[i]]
+}
+function pancakeSort(arr, sortedLength = arr.length) {
+    while (sortedLength > 0) {
+        largeIndex = getBiggest(arr, sortedLength) 
+        flip(arr, largeIndex+1) 
+        flip(arr, sortedLength) 
+        sortedLength-- 
+    }
+    return arr
 }
