4fcd96b5204be90d01638392c06ba11bbec77cae
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-03-08 18:06:46 -0700

    removed copy array code
    changed how I find the index of the next largest unsorted element and made my flip function more readable

--- code.js
+++ code.js
@@ -1,12 +1,19 @@
 function getBiggest(a, length) {
-    let b = a.slice(0,length)
-    return b.indexOf(Math.max(...b))
+    let biggest = a[0]
+    for (let i = 1; i < length; i++) {
+        if (a[i] > biggest) biggest = a[i]
+    }
+    return a.indexOf(biggest)
 }
 function flip(a, n) {
     if (n <= 1) return
     else if (n == 1) [a[0], a[1]] = [a[1], a[0]]
-    else if (n >= a.length) for (let i = 0, j = a.length-1; i < j; i++, j--) [a[i], a[j]] = [a[j], a[i]]
-    else for (let i = 0, j = n-1; i < j; i++, j--) [a[i], a[j]] = [a[j], a[i]]
+    else if (n >= a.length)
+        for (let i = 0, j = a.length-1; i < j; i++, j--) 
+            [a[i], a[j]] = [a[j], a[i]]
+    else 
+        for (let i = 0, j = n-1; i < j; i++, j--) 
+            [a[i], a[j]] = [a[j], a[i]]
 }
 function pancakeSort(arr, sortedLength = arr.length) {
     while (sortedLength > 0) {
